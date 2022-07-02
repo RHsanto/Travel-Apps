9d7cfd4031821c9d0a3df80ee665a248258fe60e
RHsanto <rakibulsanto789@gmail.com>
2022-07-02 16:32:37 +0600

    add

--- App.js
+++ App.js
@@ -31,7 +31,7 @@ export default function App() {
 
     <> 
     <NavigationContainer>
-    <Stack.Navigator>
+    <Stack.Navigator  screenOptions={{headerShown:false}}>
       <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { SafeAreaView, ScrollView, Text, View } from 'react-native';
 
 const Home = () => {
   return (
+  <SafeAreaView>
+    <ScrollView>
     <View>
       <Text>Hello</Text>
     </View>
-  );
+    </ScrollView>
+  </SafeAreaView>
+  ); 
 };
 
 export default Home;
