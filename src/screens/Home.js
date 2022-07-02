210ead559d95072adf47c0956d87b4a4ca7d1cfc
RHsanto <rakibulsanto789@gmail.com>
2022-07-02 16:48:14 +0600

    add navbar section

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,12 +1,22 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, Text, View } from 'react-native';
-
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native';
+import { spacing } from '../theme/spacing';
+import { FontAwesome } from '@expo/vector-icons';
 const Home = () => {
   return (
   <SafeAreaView>
     <ScrollView>
-    <View>
-      <Text>Hello</Text>
+{/* Container */}
+    <View style={styles.container}>
+{/* Navbar */}
+   <View style={styles.navbarView}>
+     <FontAwesome name="bars" size={32} color="black" />
+     <View>
+      <Image style={styles.profileImg}  source={require('../../assets/images/person.png')} />
+     </View>
+   </View>
+
+
     </View>
     </ScrollView>
   </SafeAreaView>
@@ -14,3 +24,18 @@ const Home = () => {
 };
 
 export default Home;
+
+ const styles=StyleSheet.create({
+ container:{
+  paddingHorizontal:spacing[5],
+ },
+ navbarView:{
+  flexDirection:'row',
+  justifyContent:'space-between',
+  alignItems:'center',
+  marginTop:spacing[8]
+ },
+ profileImg:{
+ borderRadius:10
+ }
+ })
