86c34548a2dd9169a9ea9b3e204519ecda58774d
RHsanto <rakibulsanto789@gmail.com>
2022-07-03 20:14:04 +0600

    add

--- App.js
+++ App.js
@@ -1,23 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from './src/screens/Home';
 import {
   useFonts,
-  Lato_100Thin,
-  Lato_100Thin_Italic,
-  Lato_300Light,
-  Lato_300Light_Italic,
   Lato_400Regular,
-  Lato_400Regular_Italic,
   Lato_700Bold,
-  Lato_700Bold_Italic,
-  Lato_900Black,
-  Lato_900Black_Italic,
-} from '@expo-google-fonts/lato';
-const Stack = createNativeStackNavigator();
 
-export default function App() {
+} from '@expo-google-fonts/lato';
+import Liked from './src/screens/Liked';
+import Profile from './src/screens/Profile';
+import { colors } from './src/theme/color';
+import { StyleSheet } from 'react-native';
+
+const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MyTabs=()=>{
+  return (
+    <Tab.Navigator
+    tabBarOptions={{
+      style:styles.tabBar,
+      activeTinColor:colors.orange,
+      inactiveTinColor:colors.grey
+    }}
+    >
+      <Tab.Screen name="Home" component={Home} />
+      <Tab.Screen name="Liked" component={Liked} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
+  }
+ const App =()=> {
   let [fontsLoaded] = useFonts({
     Lato_400Regular,
     Lato_700Bold,
@@ -32,7 +47,7 @@ export default function App() {
     <> 
     <NavigationContainer>
     <Stack.Navigator  screenOptions={{headerShown:false}}>
-      <Stack.Screen name="Home" component={Home} />
+      <Stack.Screen name="MyTabs" component={MyTabs} />
       </Stack.Navigator>
     </NavigationContainer>
     <StatusBar style="auto" />
@@ -41,3 +56,12 @@ export default function App() {
   );
 }
 
+export default App;
+
+const styles= StyleSheet.create({
+  tabBar:{
+    backgroundColor:colors.white,
+    borderTopLeftRadius:20,
+    borderTopRightRadius:20
+  }
+})
