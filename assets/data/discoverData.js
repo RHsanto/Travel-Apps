357cf4744941c4b71455ca247c48acb46dbc34a4
RHsanto <rakibulsanto789@gmail.com>
2022-07-04 00:49:26 +0600

    add

---
+++ assets/data/discoverData.js
@@ -0,0 +1,28 @@
+const discoverData=[
+  {
+    id:'d-1',
+    title:'Kayaking in the Tofino Sea',
+    location:'Canada',
+    image:require('../images/kayak.png'),
+    fullImage:require('../images/kayak-big.png'),
+    description:'Great day hikes and backpacking routes on the North and South Rim of this century-old national park include easy hikes overlooking the rim and more rugged trekking options that descend into the canyon.',
+    liked:true,
+    price:50,
+    rating:5,
+    duration:2
+  },
+  {
+    id:'d-2',
+    title:'Hiking the Grand Canyon ',
+    location:'USA',
+    image:require('../images/canyon.png'),
+    fullImage:require('../images/canyon (1).png'),
+    description:'Great day hikes and backpacking routes on the North and South Rim of this century-old national park include easy hikes overlooking the rim and more rugged trekking options that descend into the canyon.',
+    liked:true,
+    price:350,
+    rating:4.5,
+    duration:3
+  },
+]
+
+export default discoverData;
