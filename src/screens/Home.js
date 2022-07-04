4bfc6518e9d525304b48ea83af319555c054bc30
RHsanto <rakibulsanto789@gmail.com>
2022-07-04 15:06:03 +0600

    add

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,9 +1,36 @@
 import React from 'react';
-import { Image, Pressable, SafeAreaView, ScrollView, StyleSheet,  Text,  View }
+import { Image, Pressable,FlatList, SafeAreaView, ScrollView, StyleSheet, View, ImageBackground,TouchableOpacity }
  from 'react-native';
 import { spacing } from '../theme/spacing';
 import { FontAwesome } from '@expo/vector-icons';
+import Text from '../components/Text/Text';
+import { colors } from '../theme/color';
+import discoverData from '../../assets/data/discoverData'
 const Home = ({navigation}) => {
+    {/* here render all data */}
+    const renderDiscoverItem = ({item}) => {
+      return (
+        <TouchableOpacity
+          onPress={() =>
+            navigation.navigate('Details', {
+              item: item,
+            })
+          }>
+          <ImageBackground
+            source={item.image}
+            style={[
+              styles.discoverItem,
+              {marginLeft: item.id === 'discover-1' ? 20 : 0},
+            ]}
+            imageStyle={styles.discoverItemImage}>
+            <Text style={styles.discoverItemTitle}>{item.title}</Text>
+            <View style={styles.discoverItemLocationWrapper}>
+              <Text style={styles.discoverItemLocationText}>{item.location}</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+      );
+    };
   return (
   <SafeAreaView>
     <ScrollView>
@@ -16,16 +43,25 @@ const Home = ({navigation}) => {
       <Image style={styles.profileImg}  source={require('../../assets/images/person.png')} />
      </View>
    </View>
-  {/* details */}
-   <View>
-    <Pressable
-    onPress={()=>{
-      navigation.navigate('Details')
-    }}
-    >
-     <Text>Hello </Text>
-    </Pressable>
-   </View>
+{/* Discover */}
+    <View style={styles.discoverView}>
+       <Text preset='h1'>Discover</Text>
+     <View style={styles.discoverSubTitle}>
+        <Text style={{color:colors.orange}}>All</Text>
+        <Text style={styles.categoryText}>Destination</Text>
+        <Text style={styles.categoryText} >Cities</Text>
+        <Text style={styles.categoryText} >Experience</Text>
+     </View>
+     <View>
+     <FlatList
+      data={discoverData}
+      renderItem={renderDiscoverItem}
+      keyExtractor={(item) => item.id}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+        />
+     </View>
+    </View>
 
 
     </View>
@@ -48,5 +84,60 @@ export default Home;
  },
  profileImg:{
  borderRadius:10
- }
+ },
+ discoverView:{
+  marginTop:spacing[5]
+ },
+ discoverSubTitle:{
+  marginVertical:spacing[5],
+  flexDirection:'row',
+  justifyContent:'space-between',
+ },
+ categoryText:{
+  fontWeight:'600',
+  color:'grey'
+ },
+ discoverItem: {
+  width: 170,
+  height: 250,
+  justifyContent: 'flex-end',
+  paddingHorizontal: 10,
+  paddingVertical: 15,
+  marginRight: 20,
+},
+discoverItemImage: {
+  borderRadius: 20,
+},
+discoverItemTitle: {
+  fontFamily: 'Lato-Bold',
+  fontSize: 18,
+  color: colors.white,
+},
+discoverItemLocationWrapper: {
+  flexDirection: 'row',
+  alignItems: 'center',
+  marginTop: 5,
+},
+discoverItemLocationText: {
+  marginLeft: 5,
+  fontFamily: 'Lato-Bold',
+  fontSize: 14,
+  color: colors.white,
+},
  })
+
+
+
+
+
+
+
+ {/* <View>
+    <Pressable
+    onPress={()=>{
+      navigation.navigate('Details')
+    }}
+    >
+     <Text>Hello </Text>
+    </Pressable>
+   </View> */}
--- src/theme/color.js
+++ src/theme/color.js
@@ -3,5 +3,6 @@ export const colors ={
   black:'#0C0D0E',
   white:'#FBFCFE',
   grey:'#D1D3D2',
-  orange:'#F35D38'
+  orange:'#F35D38',
+  grey2:'#CFCFD1'
 }
