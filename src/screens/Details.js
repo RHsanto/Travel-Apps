5430a24c63f16d05d2c8309eb1882192bf0551d6
RHsanto <rakibulsanto789@gmail.com>
2022-07-27 01:58:04 +0600

    add

--- src/screens/Details.js
+++ src/screens/Details.js
@@ -1,14 +1,28 @@
 import React from 'react';
-import { SafeAreaView, ScrollView, View } from 'react-native';
+import { ImageBackground, SafeAreaView, ScrollView, StyleSheet, View } from 'react-native';
 import Text from '../components/Text/Text';
-
-const Details = () => {
+import { FontAwesome5 } from '@expo/vector-icons';
+import { colors } from '../theme/color';
+import { spacing } from '../theme/spacing';
+const Details = ({route}) => {
+  const travels =route.params.item;
+  const {id,fullImage,description,liked,price,rating,duration,location,title} =travels
   return (
     <SafeAreaView>
       <ScrollView>
-      <View>
-      <Text>Details
-Details</Text>
+      <View >
+ {/* image */}
+ <ImageBackground style={styles.detailsImage} source={fullImage}>
+  <View style={styles.detailsView}>
+   <Text preset='h1' style={styles.discoverItemTitle}>{title}</Text>
+            <View style={styles.discoverItemLocation}>
+              <FontAwesome5  name="map-marker-alt" size={15} color="white" />
+              <Text style={styles.discoverItemLocationText}>
+              {location}
+              </Text>
+            </View>
+    </View> 
+ </ImageBackground>
       </View>
       </ScrollView>
     </SafeAreaView>
@@ -16,3 +30,27 @@ Details</Text>
 };
 
 export default Details;
+
+const styles = StyleSheet.create({
+  detailsView:{
+  paddingLeft:spacing[5]
+  },
+  detailsImage:{
+    height: 450,
+  },
+  discoverItemTitle:{
+    marginTop:333,
+    color:colors.white,
+    paddingRight:spacing[5],
+    width:314
+  },
+  discoverItemLocation:{
+    marginTop:7,
+    flexDirection:'row',
+   
+  },
+  discoverItemLocationText:{
+    color:colors.white,
+    marginLeft:10
+  }
+})
