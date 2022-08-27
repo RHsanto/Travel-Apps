acf3e99ce15dc777e7d0433b8ae778b9325aba32
RHsanto <rakibulsanto789@gmail.com>
2022-08-27 14:31:57 +0600

    add

--- App.js
+++ App.js
@@ -10,7 +10,9 @@ import Home from './src/screens/Home';
 import Liked from './src/screens/Liked';
 import Profile from './src/screens/Profile';
 import Details from './src/screens/Details';
+
 const Stack = createNativeStackNavigator();
+
 const Tab = createBottomTabNavigator();
 
 const TabNavigator = () => {
